e84740129a3634e9f1009387329aae1912b47578
hadizz <hadizareoriginal@gmail.com>
2020-08-19 21:21:01 +0430

    create Components folder

--- src/Search/Country.js
+++ src/Search/Country.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { CountryWrapper, FlexRow, FlagIcon, Arrow } from './Components';
+import { CountryWrapper, FlexRow, FlagIcon, Arrow } from '../Components';
 
 const Country = ({ name, flag }) => {
   return (
