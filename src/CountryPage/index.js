e598d3ec2cd59204fe37f91b42496a9aeeee3c98
hadizz <hadizareoriginal@gmail.com>
2020-08-19 23:57:21 +0430

    fetch country full details by alpha2Code

--- src/CountryPage/index.js
+++ src/CountryPage/index.js
@@ -3,8 +3,36 @@ import { useParams, useHistory } from 'react-router-dom';
 
 const CountryPage = () => {
   const history = useHistory();
-
   const { code } = useParams();
+  const [details, setDetails] = React.useState(null);
+
+  // get country details
+
+  React.useEffect(() => {
+    async function fetchCountryDetails() {
+      try {
+        const response = await fetch(
+          `https://restcountries.eu/rest/v2/alpha/${code}`
+        );
+
+        const json = await response.json();
+        console.log('fetch data  ', json);
+
+        let value;
+        // eslint-disable-next-line
+        for (var name in json) {
+          value = json[name];
+          console.log(name, value);
+        }
+        setDetails(json);
+      } catch (error) {
+        console.log('error in fetch data coiuntry page');
+      }
+    }
+
+    fetchCountryDetails();
+  }, [code]);
+
   return (
     <div>
       <button onClick={() => history.goBack()} type="button">
