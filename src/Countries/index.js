d7d8d5678643a489aeff38fb6181220acb0b9249
hadizz <hadizareoriginal@gmail.com>
2020-08-22 04:05:51 +0430

    feat: cache all countries data(name, flag ,alpha2Code)

--- src/Countries/index.js
+++ src/Countries/index.js
@@ -8,13 +8,24 @@ function Countries() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch('https://restcountries.eu/rest/v2/all?fields=name;flag;alpha2Code')
-      .then((res) => res.json())
-      .then((data) => {
-        setCountries(data);
-        setLoading(false);
-      })
-      .catch((err) => alert('error in fetching : ', err));
+    const cachedCountries = localStorage.getItem('allCountries');
+    if (cachedCountries) {
+      console.log('we have countires, dont need to fetch it again');
+      setCountries(JSON.parse(cachedCountries));
+    } else {
+      console.log('fetching countries');
+
+      fetch('https://restcountries.eu/rest/v2/all?fields=name;flag;alpha2Code')
+        .then((res) => res.json())
+        .then((data) => {
+          let i = 0;
+          const newData = data.map((obj) => ({ ...obj, id: i++ }));
+          setCountries(newData);
+          localStorage.setItem('allCountries', JSON.stringify(newData));
+        })
+        .catch((err) => alert('error in fetching : ', err));
+    }
+    setLoading(false);
   }, []);
 
   return (
@@ -28,6 +39,7 @@ function Countries() {
         countries.map((c) => (
           <NDLink
             to={`/country/${c.alpha2Code}`}
+            key={c.id}
             // onClick={() => history.push({ pathName: '/country' })}
           >
             <CountryItem name={c.name} flag={c.flag} />
