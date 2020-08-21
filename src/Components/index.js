3c4a4b004c0a608f707362fb4677218b8a6ceee8
hadizz <hadizareoriginal@gmail.com>
2020-08-22 00:50:05 +0430

    fix(CountryPage): fix the issue that search btn doesn't work

--- src/Components/index.js
+++ src/Components/index.js
@@ -199,7 +199,7 @@ export const DataValue = styled.span`
 export const CPHeader = styled.div`
   display: flex;
   background-color: white;
-  padding: 12px 20px;
+  padding: 12px 10px;
   border-bottom: 1px solid rgba(0, 0, 0, 0.1);
 `;
 
@@ -208,13 +208,15 @@ export const CPHeaderTitle = styled.div`
   font-size: 18px;
 `;
 
-export const BackSearchBtn = styled(Link)`
+export const BackSearchBtn = styled.button`
   margin-right: auto;
   display: flex;
   flex-direction: row;
   align-items: center;
   color: #007afd;
   text-decoration: none;
+  background: white;
+  border: none;
 `;
 export const BackSearchT = styled.span`
   font-weight: 700;
