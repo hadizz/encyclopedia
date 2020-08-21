d6c133387dd25c241b48f137be16113cb58828c1
hadizz <hadizareoriginal@gmail.com>
2020-08-21 04:53:50 +0430

    docs: showing week weather states

--- src/CountryPage/WeatherSection.js
+++ src/CountryPage/WeatherSection.js
@@ -18,18 +18,29 @@ function WeatherSection({ header, data }) {
 
   console.log('in WeatherSection data : ', data);
 
+  function filterProps(d) {
+    const d1 = (({ weather_state_abbr, applicable_date }) => ({
+      applicable_date,
+      weather_state_abbr,
+    }))(d);
+    return d1;
+  }
+
   React.useEffect(() => {
     async function fetchCityWeather(id) {
       try {
         setLoading(true);
+        setError(false);
         const response = await axios.get(
           `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${id}`
         );
 
         const json = await response.data;
         console.log('fetchCityWeather data  ', json);
+        filterProps(json.consolidated_weather);
         setStates(json.consolidated_weather);
       } catch (err) {
+        setError(true);
         console.log('error in fetchCityWeather : ', err);
       } finally {
         setLoading(false);
@@ -65,21 +76,28 @@ function WeatherSection({ header, data }) {
       return 'loading';
     }
     if (error) {
-      return 'There was an erro in fetching data from server';
+      return 'There was an error in fetching data from server';
     }
     if (states === null) {
-      return 'weather states are empty';
+      return 'loading weather states';
     }
-    return states.map((state) =>
-      Object.keys(state).map((key, index) => {
-        return (
-          <DataBox key={index}>
-            <DataName>{key}</DataName>
-            <DataValue>{state[key]}</DataValue>
-          </DataBox>
-        );
-      })
-    );
+    if (states.length === 0) {
+      return 'No data found for this city';
+    }
+    return states.map((state) => {
+      const fState = filterProps(state);
+      console.log('fState', fState);
+      return (
+        <DataBox>
+          <DataName>{fState.applicable_date}</DataName>
+          <img
+            src={`https://www.metaweather.com/static/img/weather/${fState.weather_state_abbr}.svg`}
+            height="30"
+            alt="weather icon"
+          />
+        </DataBox>
+      );
+    });
   };
 
   return (
